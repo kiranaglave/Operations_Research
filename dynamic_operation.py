f3c6b379ea0ddb37b8809932791b67ff20a14b73
Clarisse Umulisa <ilibagizaclarisse@gmail.com>
2024-10-08 17:12:50 -0400

    Updates on files

--- dynamic_operation.py
+++ dynamic_operation.py
@@ -1,9 +1,29 @@
 import numpy as np
-from scipy.io import matlab
-from Constraints import mq, L, g, ms, R, mprop, mm, Jx, Jy, Jz  # Import the constants
 
 
-def non_linear(current_state, target_state, K):
+from scipy.integrate import solve_ivp
+
+from Constraints import mq, g, Jx, Jy, Jz, K, A, B # Import the constants
+
+# Initial state
+initial_state = np.zeros(12)
+
+# Current state (u, px, v, py, w, pz, phidot, phi, thetadot,theta, phsidot, phsi)
+current_state = np.zeros(12)
+u, px, v, py, w, pz, p,phi,q,theta,r,psi= np.array(current_state)
+
+# Target state (pxtarget, pytarget, pztarget are the onces only changing)
+target_state = np.zeros(12)
+
+
+# Define time span for the simulation
+time_span= [0, 10]
+
+# Control law (feedback control based on state error)
+control = -K @ (current_state - target_state)
+
+
+def non_linear(current_state, target_state, control):
     #Computes the non-linear dynamics of the drone system.
     
     #time: current time
@@ -11,18 +31,16 @@ def non_linear(current_state, target_state, K):
     #target_state: target state of the system
     #K: feedback gain matrix for the control system
     
-   #Returns the derivatives of the current state (dx).
+   #Returns (dx).
     
- # Control law (feedback control based on state error)
-    control = -K @ (current_state - target_state)
+ 
 
- # Adding gravity effect to the control (thrust in the z-direction)
+ # Adding gravity effect to the control
     control[0] += mq * g
     
     # Thrust force
     F = control[0]
 
-  
     # Linear velocity dynamics (u_dot, v_dot, w_dot)
     u_dot = r*v - q*w - g*np.sin(p)
     v_dot = p*w - r*u + g*np.cos(p)*np.sin(q)
@@ -39,7 +57,7 @@ def non_linear(current_state, target_state, K):
     return dx
 
 
-def linear(time, current_state, A, B, K, target_state):
+def linear(current_state, A, B, control, target_state):
     """
     Computes the linear dynamics of the drone system.
     
@@ -50,14 +68,13 @@ def linear(time, current_state, A, B, K, target_state):
     K: feedback gain matrix for the control system
     target_state: target state of the system
     
-    Returns the derivatives of the current state (dx).
+    Returns dx
     """
-    
-    # Control law (feedback control based on state error)
-    control = -K @ (current_state - target_state)
-    # A is given in slide 35 and B
-    
     # Linear state update: dx = Ax + B * control
     dx = A @ current_state + B @ control
     
     return dx
+
+# Solve ODEs
+non_linear_derivative = solve_ivp(non_linear, time_span, current_state, args=[target_state])
+linear_derivative = solve_ivp(linear, time_span, current_state, args=[target_state])
